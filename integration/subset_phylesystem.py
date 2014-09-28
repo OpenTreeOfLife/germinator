8e7827951fe614a7402bc140cfa9b004902df4e0
Jonathan A Rees <jar398@mumble.net>
2014-09-28 13:38:53 -0400

    Get studies from github instead of API

--- integration/subset_phylesystem.py
+++ integration/subset_phylesystem.py
@@ -6,8 +6,9 @@ import subprocess
 
 from asterales_studies import asterales_studies
 
-dirname = "asterales_phylesystem/study"
+local = "asterales_phylesystem"
 
+#for (pgnum, tree) in [(152,None)]:
 for (pgnum, tree) in asterales_studies:
 
     num_as_string = str(pgnum)
@@ -16,16 +17,19 @@ for (pgnum, tree) in asterales_studies:
     studyid = 'pg_' + num_as_string
 
     lowpart = 'pg_' + num_as_string[-2:]
+    dir = 'study/%s/%s' % (lowpart, studyid)
 
-    test_dir = "%s/%s/%s" % (dirname, lowpart, studyid)
-    if not os.path.exists(test_dir):
-        print "creating %s" % test_dir
-        os.makedirs(test_dir)
+    local_dir = "%s/%s" % (local, dir)
+    if not os.path.exists(local_dir):
+        print "creating %s" % local_dir
+        os.makedirs(local_dir)
 
     studyid_json = "%s.json" % (studyid)
+    path = "%s/%s" % (dir, studyid_json)
+    local_path = "%s/%s" % (local, path)
+    print local_path
 
-    test_path = "%s/%s" % (test_dir, studyid_json)
-    print test_path
-
-    subprocess.call(['curl', '--output', test_path, 'http://api.opentreeoflife.org/v2/study/%s' % studyid_json])
+    subprocess.call(['curl',
+                     '--output', local_path,
+                     'https://raw.githubusercontent.com/OpenTreeOfLife/phylesystem-1/master/%s' % path])
 
