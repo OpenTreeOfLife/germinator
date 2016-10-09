4d9709e4ae8be307ebdca9d4704de9da499051c7
Jonathan A Rees <jar398@mumble.net>
2016-10-09 17:14:44 -0400

    oops, missing comma

--- ws-tests/opentreetesting.py
+++ ws-tests/opentreetesting.py
@@ -274,7 +274,7 @@ translations = [('/cached/', '/phylesystem/default/cached/'),
                 ('/v3/studies/', '/db/data/ext/studies/graphdb/'),
                 ('/v2/studies/', '/db/data/ext/studies/graphdb/'),
                 # smasher (port 8081)
-                ('/v2/conflict/', '/')
+                ('/v2/conflict/', '/'),
                 ('/v3/conflict/', '/v3/')
 ]
 
