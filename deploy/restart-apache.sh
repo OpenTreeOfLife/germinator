99921381fcc42f184bda6361ecac19f39e0d045d
Jim Allman <jim@ibang.com>
2020-02-21 14:48:59 -0500

    Fixes and test logs for session sweepers
    These are deployed and working on devtree. Note that the main
    webapp-sweeper.log file is very slow to record messages, but I believe
    it is working as expected.
    
    Addresses https://github.com/OpenTreeOfLife/opentree/issues/845

--- deploy/restart-apache.sh
+++ deploy/restart-apache.sh
@@ -62,6 +62,8 @@ echo "(Re)starting web2py session sweepers..."
 # Find and kill any sweepers that are already running
 sudo pkill -f sessions2trash
 # Now run a fresh instance in the background for each webapp
-sudo nohup python $OPENTREE_HOME/web2py/web2py.py -S opentree -M -R $OPENTREE_HOME/web2py/scripts/sessions2trash.py &
+echo "Logging sweeper activity in /home/admin/*-sweeper.log"
+# TODO: After testing, send all output to /dev/null instead of these logs
+sudo nohup 1>./webapp-sweeper.log   2>./webapp-sweeper.log   python $OPENTREE_HOME/web2py/web2py.py -S opentree --verbose -M -R $OPENTREE_HOME/web2py/scripts/sessions2trash.py -A -v &
 # NOTE that we allow up to 24 hrs(!) before study-curation sessions will expire
-sudo nohup python $OPENTREE_HOME/web2py/web2py.py -S curator -M -R $OPENTREE_HOME/web2py/scripts/sessions2trash.py --expiration=86400 &
+sudo nohup 1>./curation-sweeper.log 2>./curation-sweeper.log python $OPENTREE_HOME/web2py/web2py.py -S curator  --verbose -M -R $OPENTREE_HOME/web2py/scripts/sessions2trash.py -A -v --expiration=86400 &
